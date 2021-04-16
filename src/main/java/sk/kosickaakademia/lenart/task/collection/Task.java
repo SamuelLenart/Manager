f8013f77d6314b55093d327bebad763efc9595ac
Samuel Lenart <samuel.lenart@kosickaakademia.sk>
2021-04-16 10:58:54 +0200

    Added json simple, maven driver, Collection

--- src/main/java/sk/kosickaakademia/lenart/task/collection/Task.java
+++ src/main/java/sk/kosickaakademia/lenart/task/collection/Task.java
@@ -1,4 +1,40 @@
 package sk.kosickaakademia.lenart.task.collection;
 
+import org.bson.types.ObjectId;
+import java.util.Date;
+
 public class Task {
+    private ObjectId id;
+    private String name;
+    private int priority;
+    private boolean done;
+    private Date date;
+    private double price;
+
+    public Task(String name, Date date, int priority, boolean done) {
+        this.name = name;
+        this.date = date;
+        this.priority = priority;
+        this.done = done;
+    }
+    public Task(String name, Date date, int priority, boolean done, double price) {
+        this(name, date, priority, done);
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public Date getDate() {
+        return date;
+    }
 }
--- src/main/java/sk/kosickaakademia/lenart/task/mongodb/Collection.java
+++ src/main/java/sk/kosickaakademia/lenart/task/mongodb/Collection.java
@@ -1,14 +1,33 @@
 package sk.kosickaakademia.lenart.task.mongodb;
 
 
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 import sk.kosickaakademia.lenart.task.collection.Task;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class Collection implements Mongo {
+    // name: task
+    //collection: tasks
+    private MongoClient client = new MongoClient();
+    private MongoDatabase database = client.getDatabase("task");
+    private MongoCollection<Document> collection = database.getCollection("tasks");
+
     @Override
     public void insertTask(Task task) {
-
+        Document doc = new Document();
+        doc.put("name", task.getName());
+        doc.put("date", task.getDate());
+        doc.put("priority", task.getPriority());
+        doc.put("done", task.isDone());
+        collection.insertOne(doc);
     }
 
     @Override
@@ -18,12 +37,25 @@ public class Collection implements Mongo {
 
     @Override
     public List<Task> getAllTasks() {
-        return null;
+        List<Task> list = new ArrayList<>();
+        for (Document doc : collection.find()){
+            try {
+                JSONObject object = (JSONObject) new JSONParser().parse(doc.toJson());
+                String name = (String) object.get("name");
+                Date date = (Date) object.get("date");
+                int priority = Integer.parseInt(String.valueOf(object.get("priority")));
+                boolean done = (boolean) object.get("done");
+                Task task = new Task(name, date, priority, done);
+            } catch (Exception e) { e.printStackTrace(); }
+        }
+        return list;
     }
 
-    @Override
-    public List<Task> getAllTasks(boolean done) {
-        return null;
+    public List<Task> getAllTasks(boolean done){
+        List<Task> list = new ArrayList<>();
+
+
+        return list;
     }
 
     @Override
